15bb55f156c2a031444c54479a683a9265b64af4
Cédric ZUGER <cedric.zuger@rdsdiag.com>
2021-10-31 11:39:25 +0100

    Showing bot permissions.

--- app/poll.rb
+++ app/poll.rb
@@ -81,9 +81,9 @@ def show_permissions(bot)
 
   GpLogs.debug(bot.servers.pretty_inspect, self.class, __method__)
 
-  GpLogs.debug(bot.servers[0].pretty_inspect, self.class, __method__)
+  GpLogs.debug(bot.servers[487195321852624917].pretty_inspect, self.class, __method__)
 
-  bot_profile = bot.profile.on(bot.servers[0][1])
+  bot_profile = bot.profile.on(bot.servers[487195321852624917])
 
   Discordrb::Permissions::FLAGS.each do |flag|
     flag = flag[1]
