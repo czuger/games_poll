f764dafe7f8afe3d9135f63efb0f821c9e8d38eb
ced <zuger.cedric@gmail.com>
2022-04-18 18:09:18 +0200

    Adding an init command to set the first admin.

--- app/commands/common.rb
+++ app/commands/common.rb
@@ -6,5 +6,23 @@ module Commands
       message = commands.map{ |e| "#{BOT_PREFIX}#{e[0]} - #{e[1]}"}.join("\n")
       event.channel.send_temporary_message(message, 30)
     end
+
+    # Initialize the bot. First user to run the command is admin.
+    def self.init(event, commands)
+
+      if Voter.all.count == 0
+        member = reaction_event.server.member(user_id)
+        voter_name = member.nick
+        voter_name ||= reaction_event.user.name
+
+        voter = Voter.where(discord_id: event.user.id).first_or_initialize
+        voter.name = voter_name
+        voter.admin = true
+        voter.save!
+
+        event.channel.send_temporary_message('You are now admin', 30)
+      end
+
+    end
   end
 end
